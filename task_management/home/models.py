a549c8fbcd2db8cef26152e25b12d0a317871e8a
Sahar Mahmoudishad <sahar.mahmoodi01@gmail.com>
2023-11-20 15:38:11 +0330

    refactor: modify home and base html-css

--- task_management/home/models.py
+++ task_management/home/models.py
@@ -2,17 +2,16 @@ from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    employe_rewards = models.CharField(max_length=250, choices=[("To","Time off"),("G","Gifts"),("WH","Work from home days"),("Gc","Gift cards"),("C","Certificates")],default="None")
+
 class Staff(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
     expert = models.CharField(max_length=50) 
     experience = models.IntegerField()
+    categories =models.ForeignKey(Category,on_delete=models.CASCADE,related_name='staff')
     
     
-
-class Category(models.Model):
-    title = models.CharField(max_length=50)
-    employe_rewards = models.CharField(max_length=250, choices=[("To","Time off"),("G","Gifts"),("WH","Work from home days"),("Gc","Gift cards"),("C","Certificates")],default="None")
-    staffs =models.ForeignKey(Staff,on_delete=models.CASCADE,related_name='category')
-
 
--- task_management/home/views.py
+++ task_management/home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Category
 
 
 # Create your views here.
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/home.html')
+        # allcategory=Category.objects.values_list('title', flat=True).distinct()
+        allcategory=Category.objects.all()
+        return render(request, 'home/home.html',{"allcategory":allcategory})
 
     def post(self, request):
         return render(request, 'home/home.html')
